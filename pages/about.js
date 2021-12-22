0c5325f5e24ac58922b674b5f2ae1771a8e8de7d
James Cliff <me@jamescliff.com>
2021-12-21 22:55:47 -0500

    🚀 about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import StoreHeading from "@/components/StoreHeading";
+
+function About({ products }) {
+  return (
+    <div className="mx-auto max-w-6xl">
+      <StoreHeading />
+    </div>
+  );
+}
+
+export default About;
