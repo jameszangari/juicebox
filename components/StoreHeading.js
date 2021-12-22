247aa0c38a38f8fca4f6b766c5e2bd67b55c61d8
James Cliff <me@jamescliff.com>
2021-12-21 22:54:44 -0500

    💬 copy edits

--- components/StoreHeading.js
+++ components/StoreHeading.js
@@ -1,14 +1,14 @@
-import PageTitle from '@/components/PageTitle'
+import PageTitle from "@/components/PageTitle";
 
 function StoreHeading() {
   return (
     <div className="">
-      <PageTitle text="Get Doggy Stickers!" />
+      <PageTitle text="Juicebox" />
       <p className="max-w-xl text-center px-2 mx-auto text-base text-gray-600">
-        Times are tough. Liven up your home with some cute Doggy Stickers. 🐶    
+        Age is just a number.
       </p>
     </div>
-  )
+  );
 }
 
-export default StoreHeading
+export default StoreHeading;
