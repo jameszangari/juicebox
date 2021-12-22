42ed3a3e58216f2108462d8595ac48157c9bb3be
James Cliff <me@jamescliff.com>
2021-12-21 22:53:57 -0500

    🐛 bug fixes
    - replaced react-responsive with tailwind css
    - add "100% Original" copy

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import MediaQuery from "react-responsive";
+// import MediaQuery from "react-responsive";
 import { useCartContext } from "@/context/Store";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
@@ -53,37 +53,34 @@ function Nav() {
       <nav className="sticky top-0 z-20 xp-bg">
         <div className="flex items-center justify-between w-full mx-auto px-6 py-4">
           {/* desktop menu */}
-          <MediaQuery query="(min-width: 768px)">
-            <div className="flex justify-start w-4/12">
-              <Link href="/" passHref>
-                <button className="px-4 py-2">Home</button>
-              </Link>
-              <Link href="/shop" passHref>
-                <button className="px-4 py-2">Shop</button>
-              </Link>
-              <Link href="/about" passHref>
-                <button className="px-4 py-2">About</button>
-              </Link>
-            </div>
-          </MediaQuery>
+          <div className="md:flex hidden justify-start w-4/12">
+            <Link href="/" passHref>
+              <button className="px-4 py-2">Home</button>
+            </Link>
+            <Link href="/shop" passHref>
+              <button className="px-4 py-2">Shop</button>
+            </Link>
+            <Link href="/about" passHref>
+              <button className="px-4 py-2">About</button>
+            </Link>
+          </div>
           {/* mobile menu */}
-          <MediaQuery query="(max-width: 767px)">
-            <span className="w-4/12">
-              <span
-                className={
-                  toggleMenu
-                    ? "cursor-pointer z-50 hamburger--squeeze is-active"
-                    : "cursor-pointer z-50 hamburger--squeeze"
-                }
-                type="button"
-                onClick={toggleMenuState}
-              >
-                <span className="hamburger-box">
-                  <span className="hamburger-inner"></span>
-                </span>
+          <div className="md:hidden w-4/12">
+            <span
+              className={
+                toggleMenu
+                  ? "cursor-pointer z-50 hamburger--squeeze is-active"
+                  : "cursor-pointer z-50 hamburger--squeeze"
+              }
+              type="button"
+              onClick={toggleMenuState}
+            >
+              <span className="hamburger-box">
+                <span className="hamburger-inner"></span>
               </span>
             </span>
-          </MediaQuery>
+          </div>
+          {/* Logo */}
           <div className="flex justify-center w-4/12">
             <Link href="/" passHref>
               <a className="cursor-pointer">
@@ -93,13 +90,14 @@ function Nav() {
               </a>
             </Link>
           </div>
+          {/* Cart */}
           <div className="flex justify-end w-4/12">
+            <p className="md:block hidden pr-4">100% ORIGINAL</p>
             <Link href="/cart" passHref>
               <a className=" relative" aria-label="cart">
                 <Bag />
                 {cartItems === 0 ? null : (
-                  <span className="absolute top-0 right-0 text-xs bg-black text-white font-semibold rounded-full py-1 px-2">
-                    {/* <span className="absolute top-0 right-0 text-xs bg-yellow-300 text-gray-900 font-semibold rounded-full py-1 px-2 transform translate-x-10 -translate-y-3"> */}
+                  <span className="absolute top-0 right-0 text-xs bg-black text-white font-semibold rounded-full h-6 w-6 py-1 px-2 transform translate-x-5 -translate-y-3">
                     {cartItems}
                   </span>
                 )}
