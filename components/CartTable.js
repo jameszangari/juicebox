c86a4913a4ae4773188ed94c39224d3f4c02d914
James Cliff <me@jamescliff.com>
2022-01-11 13:07:26 -0500

    fix: remove cart table when cart is empty

--- components/CartTable.js
+++ components/CartTable.js
@@ -8,7 +8,6 @@ function CartTable({ cart }) {
   const updateCartQuantity = useUpdateCartQuantityContext();
   const [cartItems, setCartItems] = useState([]);
   const [subtotal, setSubtotal] = useState(0);
-
   useEffect(() => {
     setCartItems(cart);
     setSubtotal(getCartSubTotal(cart));
@@ -19,84 +18,99 @@ function CartTable({ cart }) {
   }
 
   return (
-    <div className="min-h-80 my-4 sm:my-8 mx-auto w-full">
-      <table className="max-w-6xl mx-auto w-3/4">
-        <thead>
-          <tr className="uppercase text-xs sm:text-sm border-b border-jbgray">
-            <th className="font-primary font-normal px-6 py-4">Product</th>
-            <th className="font-primary font-normal px-6 py-4">Quantity</th>
-            <th className="font-primary font-normal px-6 py-4 hidden sm:table-cell">
-              Price
-            </th>
-            <th className="font-primary font-normal px-6 py-4">Remove</th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-jblightgray">
-          {cartItems.map((item) => (
-            <tr
-              key={item.variantId}
-              className="text-sm sm:text-base text-gray-600 text-center"
-            >
-              <td className="font-primary font-medium px-4 sm:px-6 py-4 flex items-center">
-                <img
-                  src={item.productImage.originalSrc}
-                  alt={item.productImage.altText}
-                  height={64}
-                  width={64}
-                  className={`sm:inline-flex`}
-                />
-                <Link passHref href={`/products/${item.productHandle}`}>
-                  <a className="pt-1 hover:text-jbblue2">
-                    {item.productTitle}, {item.variantTitle}
-                  </a>
-                </Link>
-              </td>
-              <td className="font-primary font-medium px-4 sm:px-6 py-4">
-                <input
-                  type="number"
-                  inputMode="numeric"
-                  id="variant-quantity"
-                  name="variant-quantity"
-                  min="1"
-                  step="1"
-                  value={item.variantQuantity}
-                  onChange={(e) => updateItem(item.variantId, e.target.value)}
-                  className="text-gray-900 form-input border border-gray-300 w-16 rounded-sm focus:border-jbgray focus:ring-jbgray"
-                />
-              </td>
-              <td className="font-primary text-base font-light px-4 sm:px-6 py-4 hidden sm:table-cell">
-                <Price currency="$" num={item.variantPrice} numSize="text-lg" />
-              </td>
-              <td className="font-primary font-medium px-4 sm:px-6 py-4">
-                <a
-                  aria-label="delete-item"
-                  className="cursor-pointer hover:opacity-80 transition-all"
-                  onClick={() => updateItem(item.variantId, 0)}
+    <>
+      {cart >= 0 && (
+        <p className="text-xl font-primary text-center mt-8 mb-16">
+          Your box is empty.
+        </p>
+      )}
+      {cartItems != 0 && (
+        <div className="min-h-80 my-4 sm:my-8 mx-auto w-full">
+          <table className="max-w-6xl mx-auto w-3/4">
+            <thead>
+              <tr className="uppercase text-xs sm:text-sm border-b border-jbgray">
+                <th className="font-primary font-normal px-6 py-4">Product</th>
+                <th className="font-primary font-normal px-6 py-4">Quantity</th>
+                <th className="font-primary font-normal px-6 py-4 hidden sm:table-cell">
+                  Price
+                </th>
+                <th className="font-primary font-normal px-6 py-4">Remove</th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-jblightgray">
+              {cartItems.map((item) => (
+                <tr
+                  key={item.variantId}
+                  className="text-sm sm:text-base text-gray-600 text-center"
                 >
-                  <img
-                    src="/images/recycle.ico"
-                    alt="remove"
-                    className="block m-auto"
-                  />
-                </a>
-              </td>
-            </tr>
-          ))}
-          {subtotal === 0 ? null : (
-            <tr className="text-center">
-              <td></td>
-              <td className="font-primary text-base text-gray-600 font-semibold uppercase px-4 sm:px-6 py-4">
-                Subtotal
-              </td>
-              <td className="font-primary text-lg text-jbblue font-medium px-4 sm:px-6 py-4">
-                <Price currency="$" num={subtotal} numSize="text-xl" />
-              </td>
-              <td></td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
+                  <td className="font-primary font-medium px-4 sm:px-6 py-4 flex items-center">
+                    <img
+                      src={item.productImage.originalSrc}
+                      alt={item.productImage.altText}
+                      height={64}
+                      width={64}
+                      className={`sm:inline-flex`}
+                    />
+                    <Link passHref href={`/products/${item.productHandle}`}>
+                      <a className="pt-1 hover:text-jbblue2">
+                        {item.productTitle}, {item.variantTitle}
+                      </a>
+                    </Link>
+                  </td>
+                  <td className="font-primary font-medium px-4 sm:px-6 py-4">
+                    <input
+                      type="number"
+                      inputMode="numeric"
+                      id="variant-quantity"
+                      name="variant-quantity"
+                      min="1"
+                      step="1"
+                      value={item.variantQuantity}
+                      onChange={(e) =>
+                        updateItem(item.variantId, e.target.value)
+                      }
+                      className="text-gray-900 form-input border border-gray-300 w-16 rounded-sm focus:border-jbgray focus:ring-jbgray"
+                    />
+                  </td>
+                  <td className="font-primary text-base font-light px-4 sm:px-6 py-4 hidden sm:table-cell">
+                    <Price
+                      currency="$"
+                      num={item.variantPrice}
+                      numSize="text-lg"
+                    />
+                  </td>
+                  <td className="font-primary font-medium px-4 sm:px-6 py-4">
+                    <a
+                      aria-label="delete-item"
+                      className="cursor-pointer hover:opacity-80 transition-all"
+                      onClick={() => updateItem(item.variantId, 0)}
+                    >
+                      <img
+                        src="/images/recycle.ico"
+                        alt="remove"
+                        className="block m-auto"
+                      />
+                    </a>
+                  </td>
+                </tr>
+              ))}
+              {subtotal === 0 ? null : (
+                <tr className="text-center">
+                  <td></td>
+                  <td className="font-primary text-base text-gray-600 font-semibold uppercase px-4 sm:px-6 py-4">
+                    Subtotal
+                  </td>
+                  <td className="font-primary text-lg text-jbblue font-medium px-4 sm:px-6 py-4">
+                    <Price currency="$" num={subtotal} numSize="text-xl" />
+                  </td>
+                  <td></td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
   );
 }
 
--- pages/cart.js
+++ pages/cart.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import SEO from "@/components/SEO";
 import PageTitle from "@/components/PageTitle";
 import CartTable from "@/components/CartTable";
@@ -7,15 +8,22 @@ import { useCartContext } from "@/context/Store";
 function CartPage() {
   const pageTitle = `box | ${process.env.siteTitle}`;
   const [cart, checkoutUrl] = useCartContext();
-
+  console.log(cart);
   return (
     <div className="container mx-auto mb-20 min-h-screen">
       <SEO title={pageTitle} />
       <PageTitle text="What's in your Box?" />
       <CartTable cart={cart} />
-      <div className="max-w-sm mx-auto space-y-4 px-2">
-        <CheckOutButton webUrl={checkoutUrl} />
-      </div>
+      {cart >= 0 && (
+        <Link href="/shop" passHref>
+          <button className="px-10 py-2 m-auto block">Return to Shop</button>
+        </Link>
+      )}
+      {cart != 0 && (
+        <div className="max-w-sm mx-auto space-y-4 px-2">
+          <CheckOutButton webUrl={checkoutUrl} />
+        </div>
+      )}
     </div>
   );
 }
